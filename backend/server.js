30a935e343028837f3033c8b5b460a2abc542d6d
Leandro Reis <leandro.reiz@gmail.com>
2022-04-22 14:25:42 +0100

    Add database connection and create user on database

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const colors = require('colors');
+const connectDB = require('./config/db');
 const { errorHandler } = require('./middleware/errorMiddleware');
 const PORT = process.env.PORT || 5000;
+
+// Connect to database
+connectDB();
 
 const app = express();
 
